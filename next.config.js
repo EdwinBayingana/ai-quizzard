8eb8a11e8b6dbd955c244f9700fe8c58b27f6282
EdwinBayingana <bayinganaedwin@gmail.com>
2023-11-11 00:56:47 +0200

    add the vercel.json file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
-  // },
-  // experimental: {
-  //   output: 'standalone',
-  //   distDir: 'dist',
-  // },
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  },
+  experimental: {
+    output: 'standalone',
+    distDir: 'dist',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -16,20 +16,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   output: 'standalone',
-//   distDir: 'dist',
-// };
-
-// module.exports = nextConfig;
