68559fa472ddf18c24bc6058d41caa1b0bd919cd
EdwinBayingana <bayinganaedwin@gmail.com>
2023-11-11 00:24:34 +0200

    update the next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
+  // images: {
+  //   domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  // },
+  // experimental: {
+  //   output: 'standalone',
+  //   distDir: 'dist',
+  // },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // output: 'export',
-  distDir: 'dist',
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   output: 'standalone',
+//   distDir: 'dist',
+// };
+
+// module.exports = nextConfig;
